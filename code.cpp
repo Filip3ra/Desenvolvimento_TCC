e00dc9185ee79e238a024547218d35b82f7b56ab
Filip3ra <filipimacielrj@gmail.com>
2024-11-04 09:39:19 -0300

    organizando arquivos

---
+++ code.cpp
@@ -0,0 +1,6 @@
+#include "header.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include "header.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+  if (argc < 2)
+  {
+    cout << "Informe o nome do arquivo: ./program [filename]" << endl;
+    return 0;
+  }
+  string nome_arquivo = argv[1];
+}
