cb28afc5a05672a23851ca050273372443b1a868
Filip3ra <filipimacielrj@gmail.com>
2024-11-05 15:33:58 -0300

    lendo arquivo e organizando

--- header.hpp
+++ header.hpp
@@ -1,5 +1,59 @@
 #pragma once
+
+#include <iostream>
+#include <string>
 #include <vector>
-#include <utility>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <stack>
 
 using namespace std;
+
+// matriz: sequence of ops in the machines
+#define matriz vector<vector<int>>
+
+// Schedule: sequence of ops in the machines + start times
+#define Schedule pair<matriz, vector<int>>
+
+// Neighbor: Schedule + move that created the neighbor
+#define Neighbor pair<Schedule, vector<int>>
+
+class JIT_JSS
+{
+
+  // operations start from 1
+  // jobs and machines start from 0 (the machines are 0-indexed in the instances of Baptiste et. al(2008))
+
+public:
+  // number of jobs in the instance
+  int nJobs;
+  // number of machines in the instance
+  int nMachines;
+  // number of operations
+  int nOperations;
+  // processingTime[i] = processing time of operation i
+  vector<int> processingTime;
+  // duedDate[i] = due date of operation i
+  vector<int> dueDate;
+  // earliness[i] = earliness penalty of operation i
+  vector<double> earliness;
+  // tardiness[i] = tardiness penalty of operation i
+  vector<double> tardiness;
+  // job[i] = job of operation i (job index starts from 0)
+  vector<int> job;
+  // machine[i] = machine of op i (machine index starts from 0)
+  vector<int> machine;
+  // jobOps[i] = operations of job i
+  matriz jobOps;
+  // machineOps[i] = operations of machine i
+  matriz machineOps;
+  // processingOrder[i] = processing order of job i
+  matriz processingOrder;
+
+  vector<double> readLine(string line);
+
+  void parseInstance(string path);
+
+  void printInstance();
+};
--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <queue>
 #include "header.hpp"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 
-  if (argc < 2)
+  if (argc == 0)
   {
-    cout << "Informe o nome do arquivo: ./program [filename]" << endl;
-    return 0;
+    cout << "Arguments where not provided\n";
+    return -1;
   }
-  string nome_arquivo = argv[1];
+
+  string file_path(argv[1]);
+
+  JIT_JSS j;
+
+  j.parseInstance(file_path);
+  j.printInstance();
 }
