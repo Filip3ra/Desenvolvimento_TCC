2015f018d2fd2fcc40192aee8a59855418e46fe3
Filip3ra <filipimacielrj@gmail.com>
2024-11-13 15:49:41 -0300

    fitness funcionando

--- brkga.cpp
+++ brkga.cpp
@@ -70,35 +70,99 @@ vector<vector<int>> GeneratePopulation(JIT &j, int N)
 
 void Fitness(JIT &j, vector<vector<int>> population)
 {
-  int sizeVet = j.nJobs * j.nMachines;
-  int op1, op2 = 0;
-  vector<int> jobsVet;
-  int sizePopulaion = population.size();
 
-  for (int i = 0; i < sizePopulaion; i++)
+  for (const auto &jobsVet : population)
   {
+    // Controle de operações processadas e tempos de término
+    vector<bool> lastCompletionTime(j.nJobs, false); // Tempo de término da última operação de cada job
+    double totalCost = 0.0;
+    int actualCompletionTime = 0;
 
-    cout << "ind " << i + 1 << ": [";
-    // indivíduo i
-    jobsVet = population[i];
-    // elementos do indivíduo i
-    for (int j : jobsVet)
-      cout << j << " ";
+    for (int i = 0; i < jobsVet.size(); i++)
+    {
+      int currentJob = jobsVet[i] - 1; // Índice do job no vetor de dados
+      int opIndex = 0;                 // Índice da operação atual para o job
 
-    cout << "] " << endl;
-    cout << "p = " << i << endl;
+      // Controle pra saber se já procesei a primeira operação ou não
+      if (lastCompletionTime[currentJob] == false)
+      {
+        opIndex = 0;
+        lastCompletionTime[currentJob] = true;
+      }
+      else
+      {
+        opIndex = 1;
+      }
 
-    op1 = j.processingOrder[jobsVet[i] - 1][0];
-    op2 = j.processingOrder[jobsVet[i] - 1][1];
+      // Obter a posição da operação atual do job nos vetores de dados
+      int op = j.processingOrder[currentJob][opIndex];
+      int machine = j.machine[op];
+      int procTime = j.processingTime[op];
+      int dueDate = j.dueDate[op];
+      double alpha = j.earliness[op];
+      double beta = j.tardiness[op];
 
-    // int currentJob = jobsVet[i] - 1; // índice zero
-    cout << "job " << jobsVet[i] << ": <" << op1 << ", " << op2 << ">"
-         << " op1: " << j.machine[op1]
-         << " processing time: " << j.processingTime[op1]
-         << " op2: " << j.machine[op2]
-         << " processing time: " << j.processingTime[op2]
-         << "\n\n";
+      // Calcular o tempo de início considerando o término da última operação calculada
+      int startTime = actualCompletionTime;
+      actualCompletionTime += procTime;
+      int completionTime = actualCompletionTime;
+
+      // Calcular penalidades de adiantamento e atraso
+      int earliness = max(dueDate - completionTime, 0);
+      int tardiness = max(completionTime - dueDate, 0);
+      double penaltyCost = alpha * earliness + beta * tardiness;
+
+      // Acumular penalidade no custo total
+      totalCost += penaltyCost;
+
+      // Exibir detalhes para cada operação
+      cout << "Job " << currentJob + 1 << " - Operacao " << opIndex + 1
+           << ": Machine " << machine << ", Start " << startTime
+           << ", Completion " << completionTime
+           << ", Earliness " << earliness
+           << ", Tardiness " << tardiness
+           << ", Penalty Cost " << penaltyCost << endl;
+    }
+
+    // Exibir o custo total para esta sequência de jobsVet
+    cout << "Custo total para esta sequencia: " << totalCost << endl;
   }
+
+  // return totalCost;
+
+  /*
+    // SÓ PRA IMPRIMIR
+    int sizeVet = j.nJobs * j.nMachines;
+    int op1, op2 = 0;
+    vector<int> jobsVet;
+    int sizePopulaion = population.size();
+    // Consigo acessar corretamente, aqui é só pra imprimir
+    for (int i = 0; i < sizePopulaion; i++)
+    {
+      cout << "ind " << i + 1 << ": [";
+      // indivíduo i
+      jobsVet = population[i];
+      // elementos do indivíduo i
+      for (int j : jobsVet)
+        cout << j << " ";
+
+      cout << "] " << endl;
+      cout << "p = " << i << endl;
+
+
+
+      op1 = j.processingOrder[jobsVet[i] - 1][0];
+      op2 = j.processingOrder[jobsVet[i] - 1][1];
+
+      // int currentJob = jobsVet[i] - 1; // índice zero
+      cout << "job " << jobsVet[i] << ": <" << op1 << ", " << op2 << ">"
+           << " op1: " << j.machine[op1]
+           << " processing time: " << j.processingTime[op1]
+           << " op2: " << j.machine[op2]
+           << " processing time: " << j.processingTime[op2]
+           << "\n\n";
+    }
+    */
 }
 
 /*
