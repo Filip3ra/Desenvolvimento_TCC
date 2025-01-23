07e819c3fb1066cfcb7c4c2fd958793d9816345a
Filip3ra <filipimacielrj@gmail.com>
2025-01-23 12:57:52 -0300

    teste rápido

--- main.cpp
+++ main.cpp
@@ -15,19 +15,19 @@ int main(int argc, char **argv)
 
   string outputFile(argv[1]);
 
-  /* para testes rápidos
-    vector<string> folders = {"loose-equal"};
-    vector<string> files = {"test1_10x2.txt", "test2_10x2.txt", "test1_10x5.txt", "test2_10x5.txt"};
+  /* para testes rápidos */
+  vector<string> folders = {"loose-equal"};
+  vector<string> files = {"test1_10x2.txt", "test2_10x2.txt", "test1_10x5.txt", "test2_10x5.txt"};
+
+  /*
+    vector<string> folders = {"loose-equal", "loose-tard", "tight-equal", "tight-tard"};
+    vector<string> files = {
+        "test1_10x2.txt", "test2_10x2.txt", "test1_10x5.txt", "test2_10x5.txt",
+        "test1_10x10.txt", "test2_10x10.txt", "test1_15x2.txt", "test2_15x2.txt",
+        "test1_15x5.txt", "test2_15x5.txt", "test1_15x10.txt", "test2_15x10.txt",
+        "test1_20x2.txt", "test2_20x2.txt", "test1_20x5.txt", "test2_20x5.txt",
+        "test1_20x10.txt", "test2_20x10.txt"};
   */
-
-  vector<string> folders = {"loose-equal", "loose-tard", "tight-equal", "tight-tard"};
-  vector<string> files = {
-      "test1_10x2.txt", "test2_10x2.txt", "test1_10x5.txt", "test2_10x5.txt",
-      "test1_10x10.txt", "test2_10x10.txt", "test1_15x2.txt", "test2_15x2.txt",
-      "test1_15x5.txt", "test2_15x5.txt", "test1_15x10.txt", "test2_15x10.txt",
-      "test1_20x2.txt", "test2_20x2.txt", "test1_20x5.txt", "test2_20x5.txt",
-      "test1_20x10.txt", "test2_20x10.txt"};
-
   int executions = 3;    // Número de execuções
   int individuals = 481; // Quantidade de indivíduos
   int generations = 514; // Número de gerações
